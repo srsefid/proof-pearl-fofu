2722dde30887a3caa626d06158c328ca8c6b7d95
Reza Sefidgar <reza.sefidgar@tum.de>
2017-02-01 20:06:44 +0100

    relation of saturating push and unsat potential

--- formalization/Dinic_Maxflow/Preflow_Complexity_Test.thy
+++ formalization/Dinic_Maxflow/Preflow_Complexity_Test.thy
@@ -43,77 +43,225 @@ proof -
   finally show ?thesis by auto
 qed
   
-lemma
+lemma sum_arb:
+  assumes A_fin: "finite A"
+      and x_mem: "x \<in> A" 
+      and x_dif: "\<forall>y\<in>A. y \<noteq> x \<longrightarrow> g y = h y"
+    shows "(\<Sum>a\<in>A. g a) = (\<Sum>a\<in>A - {x}. h a) + g x"
+proof -
+  have "A = (A - {x}) \<union> {x}" using x_mem by auto
+  moreover note sum.union_disjoint[of "A - {x}" "{x}" g]
+  moreover note sum.cong[of "A - {x}" "A - {x}" g h]
+  ultimately show ?thesis using A_fin x_dif by auto
+qed
+  
+lemma push_excess_out:
+  assumes "(u, v) \<in> cf.E"
+  defines X_def:"X \<equiv> min (excess f u) (cf (u,v))"  
+    shows "excess (push_effect f (u, v)) u = excess f u - X"
+proof -
+  have "(u, v)\<in>E \<or> (v, u)\<in>E" using assms cfE_ss_invE by fastforce
+  thus ?thesis 
+  proof 
+    assume "(u, v) \<in> E"
+    then have "(v, u) \<notin> E" using no_parallel_edge by blast
+        
+    define f' where "f' \<equiv> push_effect f (u, v)"
+    then have[simp]: "f' = f( (u,v) := f (u,v) + X )" 
+      unfolding f'_def push_effect_def augment_edge_def X_def using `(u, v) \<in> E` by simp  
+        
+    { 
+      have f1:"(u, v) \<in> outgoing u" unfolding outgoing_def using `(u, v) \<in> E` by simp
+      have f2: "\<forall>e \<in> (outgoing u). e \<noteq> (u, v) \<longrightarrow> f' e = f e" by simp
+      have f3: "\<forall>e \<in> (outgoing u). e \<noteq> (u, v) \<longrightarrow> f e = f e"  by blast
+      
+      note sum_arb[OF finite_outgoing[OF finite_V] f1 f2]
+      moreover note sum_arb[OF finite_outgoing[OF finite_V] f1 f3, symmetric]
+      ultimately have "(\<Sum>e\<in>outgoing u. f' e) = (\<Sum>e\<in>outgoing u. f e) + X" by auto
+    }
+    moreover {    
+      have "\<forall>e \<in> (incoming u). e \<noteq> (u, v) \<longrightarrow> f' e = f e" by simp
+      moreover have "(u, v) \<notin> (incoming u)" unfolding incoming_def using `(v, u) \<notin> E` by auto
+      ultimately have "\<forall>e \<in> (incoming u). f' e = f e" by auto
+      then have "(\<Sum>e\<in>incoming u. f' e) = (\<Sum>e\<in>incoming u. f e)" by auto 
+    }
+    ultimately have "excess f' u = excess f u - X" unfolding excess_def by auto        
+    thus ?thesis unfolding f'_def by simp
+  next
+    assume "(v, u) \<in> E"
+    then have "(u, v) \<notin> E" using no_parallel_edge by blast
+        
+    define f' where "f' \<equiv> push_effect f (u, v)"
+    then have[simp]: "f' = f( (v,u) := f (v,u) - X )" 
+      unfolding f'_def push_effect_def augment_edge_def X_def using `(v, u) \<in> E` `(u, v) \<notin> E` by simp
+        
+    { 
+      have f1:"(v, u) \<in> incoming u" unfolding incoming_def using `(v, u) \<in> E` by simp
+      have f2: "\<forall>e \<in> (incoming u). e \<noteq> (v, u) \<longrightarrow> f' e = f e" by simp
+      have f3: "\<forall>e \<in> (incoming u). e \<noteq> (v, u) \<longrightarrow> f e = f e"  by blast
+      
+      note sum_arb[OF finite_incoming[OF finite_V] f1 f2]
+      moreover note sum_arb[OF finite_incoming[OF finite_V] f1 f3, symmetric]
+      ultimately have "(\<Sum>e\<in>incoming u. f' e) = (\<Sum>e\<in>incoming u. f e) - X" by auto
+    }
+    moreover {    
+      have "\<forall>e \<in> (outgoing u). e \<noteq> (v, u) \<longrightarrow> f' e = f e" by simp
+      moreover have "(v, u) \<notin> (outgoing u)" unfolding outgoing_def using `(u, v) \<notin> E` by auto
+      ultimately have "\<forall>e \<in> (outgoing u). f' e = f e" by auto
+      then have "(\<Sum>e\<in>outgoing u. f' e) = (\<Sum>e\<in>outgoing u. f e)" by auto 
+    }
+    ultimately have "excess f' u = excess f u - X" unfolding excess_def by auto        
+    thus ?thesis unfolding f'_def by simp
+  qed
+qed
+  
+lemma push_excess_in:
+  assumes "(u, v) \<in> cf.E"
+  defines X_def:"X \<equiv> min (excess f u) (cf (u,v))"  
+    shows "excess (push_effect f (u, v)) v = excess f v + X"
+proof -
+  have "(u, v)\<in>E \<or> (v, u)\<in>E" using assms cfE_ss_invE by fastforce
+  thus ?thesis 
+  proof 
+    assume "(u, v) \<in> E"
+    then have "(v, u) \<notin> E" using no_parallel_edge by blast
+        
+    define f' where "f' \<equiv> push_effect f (u, v)"
+    then have[simp]: "f' = f( (u,v) := f (u,v) + X )" 
+      unfolding f'_def push_effect_def augment_edge_def X_def using `(u, v) \<in> E` by simp  
+        
+    { 
+      have f1:"(u, v) \<in> incoming v" unfolding incoming_def using `(u, v) \<in> E` by simp
+      have f2: "\<forall>e \<in> (incoming v). e \<noteq> (u, v) \<longrightarrow> f' e = f e" by simp
+      have f3: "\<forall>e \<in> (incoming v). e \<noteq> (u, v) \<longrightarrow> f e = f e"  by blast
+      
+      note sum_arb[OF finite_incoming[OF finite_V] f1 f2]
+      moreover note sum_arb[OF finite_incoming[OF finite_V] f1 f3, symmetric]
+      ultimately have "(\<Sum>e\<in>incoming v. f' e) = (\<Sum>e\<in>incoming v. f e) + X" by auto
+    }
+    moreover {    
+      have "\<forall>e \<in> (outgoing v). e \<noteq> (u, v) \<longrightarrow> f' e = f e" by simp
+      moreover have "(u, v) \<notin> (outgoing v)" unfolding outgoing_def using `(v, u) \<notin> E` by auto
+      ultimately have "\<forall>e \<in> (outgoing v). f' e = f e" by auto
+      then have "(\<Sum>e\<in>outgoing v. f' e) = (\<Sum>e\<in>outgoing v. f e)" by auto 
+    }
+    ultimately have "excess f' v = excess f v + X" unfolding excess_def by auto        
+    thus ?thesis unfolding f'_def by simp
+  next
+    assume "(v, u) \<in> E"
+    then have "(u, v) \<notin> E" using no_parallel_edge by blast
+        
+    define f' where "f' \<equiv> push_effect f (u, v)"
+    then have[simp]: "f' = f( (v,u) := f (v,u) - X )" 
+      unfolding f'_def push_effect_def augment_edge_def X_def using `(v, u) \<in> E` `(u, v) \<notin> E` by simp
+        
+    { 
+      have f1:"(v, u) \<in> outgoing v" unfolding outgoing_def using `(v, u) \<in> E` by simp
+      have f2: "\<forall>e \<in> (outgoing v). e \<noteq> (v, u) \<longrightarrow> f' e = f e" by simp
+      have f3: "\<forall>e \<in> (outgoing v). e \<noteq> (v, u) \<longrightarrow> f e = f e"  by blast
+      
+      note sum_arb[OF finite_outgoing[OF finite_V] f1 f2]
+      moreover note sum_arb[OF finite_outgoing[OF finite_V] f1 f3, symmetric]
+      ultimately have "(\<Sum>e\<in>outgoing v. f' e) = (\<Sum>e\<in>outgoing v. f e) - X" by auto
+    }
+    moreover {    
+      have "\<forall>e \<in> (incoming v). e \<noteq> (v, u) \<longrightarrow> f' e = f e" by simp
+      moreover have "(v, u) \<notin> (incoming v)" unfolding incoming_def using `(u, v) \<notin> E` by auto
+      ultimately have "\<forall>e \<in> (incoming v). f' e = f e" by auto
+      then have "(\<Sum>e\<in>incoming v. f' e) = (\<Sum>e\<in>incoming v. f e)" by auto 
+    }
+    ultimately have "excess f' v = excess f v + X" unfolding excess_def by auto        
+    thus ?thesis unfolding f'_def by simp
+  qed
+qed
+  
+lemma sat_push_unsat_potential:
   assumes "sat_push_precond f l e"
   shows "unsat_potential (push_effect f e) l \<le> 2 * card V + unsat_potential f l"
 proof - 
-  obtain u v where obt:"e = (u, v)" by (cases e) auto
+  obtain u v where obt:"e = (u, v)" by (cases e) auto   
   
-  have "\<lbrakk>e1 \<noteq> (u, v); e1\<noteq> (v, u)\<rbrakk> \<Longrightarrow> (push_effect f e) e1 = f e1" for e1
-    unfolding push_effect_def augment_edge_def using obt by (auto split:if_split)
+  have u_inV: "u \<in> V" using assms obt excess_nodes_only unfolding sat_push_precond_def by blast
+  have v_inV: "v \<in> V" using assms obt unfolding sat_push_precond_def using  cf.V_def by auto
+  have u_not_v: "u \<noteq> v" using assms obt unfolding sat_push_precond_def by auto
+  have unsat_pot_decomp: "{x\<in>V. P x} = ({x\<in>V - {u, v}. P x}) \<union> {x\<in>{u,v}. P x}" for P
+    using u_inV v_inV by auto
+  
+  note unsat_pot_decomp[of "\<lambda>x. excess (push_effect f e) x > 0"] 
   then have 
-    "\<lbrakk>w \<noteq> u; w \<noteq> v; e1 \<in> incoming w\<rbrakk> \<Longrightarrow> (push_effect f (u, v)) e1 = f e1" and
-    "\<lbrakk>w \<noteq> u; w \<noteq> v; e1 \<in> outgoing w\<rbrakk> \<Longrightarrow> (push_effect f (u, v)) e1 = f e1" for w e1
-    unfolding incoming_def outgoing_def using obt by auto
-  then have "\<lbrakk>w \<noteq> u; w \<noteq> v\<rbrakk> \<Longrightarrow> (\<Sum>e\<in>incoming w. (push_effect f (u, v)) e) = (\<Sum>e\<in>incoming w. f e)"
-    and "\<lbrakk>w \<noteq> u; w \<noteq> v\<rbrakk> \<Longrightarrow> (\<Sum>e\<in>outgoing w. (push_effect f (u, v)) e) = (\<Sum>e\<in>outgoing w. f e)" 
-    for w by auto   
-  then have "\<lbrakk>w \<noteq> u; w \<noteq> v\<rbrakk> \<Longrightarrow> excess (push_effect f e) w = excess f w" for w
-    unfolding excess_def using obt by simp
-      
-  
-      
-  (*    
-      
-      have ttt:"min (excess f u) (cf (u, v)) = cf (u, v)" using assms unfolding sat_push_precond_def obt by auto 
-   
-  
-          
-          
-  have "\<lbrakk>x \<noteq> u; x \<noteq> v\<rbrakk> \<Longrightarrow> excess (push_effect f e) x = excess f x " for x
-    unfolding push_effect_def augment_edge_def using obt no_parallel_edge 
-    apply (clarsimp simp add: ttt split:if_split)
-    unfolding excess_def 
-    apply (auto simp add: incoming_alt outgoing_alt split:if_split)
-  
-      
-      oops
-  let ?X1 = "(if excess f u > 0 then l u else 0)"
-  let ?X2 = "(if excess f v > 0 then l v else 0)"
-      
-  have sep:"y \<in> V \<Longrightarrow> unsat_potential f l = (\<Sum>x\<in>{v\<in>V. excess f v > 0} - {y}. l x) + 
-    (if excess f y > 0 then l y else 0)" for f y
-  proof (auto simp add: unsat_potential_def split: if_splits)
-    assume asm1:"y \<in> V"
-    assume asm2:"0 < excess f y"
-    
-    have "({v\<in>V. excess f v > 0} - {y}) \<union> {y} = {v\<in>V. excess f v > 0}" using asm1 asm2 by auto
-    then show "sum l {v \<in> V. 0 < excess f v} = sum l ({v \<in> V. 0 < excess f v} - {y}) + l y"
-      using sum.union_disjoint[of "{v \<in> V. 0 < excess f v} - {y}" "{y}"]  by auto
-  qed
-    
-    find_theorems sat_push_precond adm_edges
-    
-  have "u \<in> V" using assms obt excess_nodes_only unfolding sat_push_precond_def by blast
-  then have "unsat_potential f l = (\<Sum>x\<in>{v\<in>V. excess f v > 0} - {u}. l x) + ?X1" using sep by auto
-  moreover have "(\<Sum>x\<in>{v\<in>V. excess f v > 0} - {u} - {v}. l x) =
-   (\<Sum>x\<in>{v\<in>V. excess f v > 0} - {u} - {v}. l x) + ?X1 + ?X2" (is "_ = ?S + _ + _")
+    "unsat_potential (push_effect f e) l =  sum l {x \<in> V-{u,v}. 0 < excess (push_effect f e) x} +
+    sum l {x\<in>{u,v}. 0 < excess (push_effect f e) x}" (is "_ = ?R1 + ?R2")unfolding unsat_potential_def
+    using sum.union_disjoint[of "{x \<in> V-{u,v}. 0 < excess (push_effect f e) x}"
+      "{x \<in> {u,v}. 0 < excess (push_effect f e) x}" l] by auto
+  also have "?R1 = sum l {x \<in> V - {u, v}. 0 < excess f x}" (is "_ = ?R1'")
   proof -
-    have "v \<noteq> u" using assms unfolding sat_push_precond_def cf.E_def using obt by auto
-    moreover have "v \<in> V" using assms obt unfolding sat_push_precond_def using  cf.V_def by auto
-    ultimately have "v \<in> {v\<in>V. excess f v > 0} - {u}" 
-
-oops
-and 
-  then have "unsat_potential f l = (\<Sum>x\<in>{v\<in>V. excess f v > 0} - {u} - {v}. l x) + ?X"
-    using sep sledgehammer
-*)    
-  show ?thesis sorry
-qed
-      
+    {
+      fix w e'
+      assume "w \<noteq> u"
+      assume "w \<noteq> v"
+      have "\<lbrakk>e1 \<noteq> (u, v); e1\<noteq> (v, u)\<rbrakk> \<Longrightarrow> (push_effect f e) e1 = f e1" for e1
+        unfolding push_effect_def augment_edge_def using obt by (auto split:if_split)
+      then have "e1 \<in> incoming w \<Longrightarrow> (push_effect f (u, v)) e1 = f e1"
+        and "e1 \<in> outgoing w \<Longrightarrow> (push_effect f (u, v)) e1 = f e1" for e1
+        unfolding incoming_def outgoing_def using obt `w \<noteq> u` `w \<noteq> v` by auto
+      then have "(\<Sum>e\<in>incoming w. (push_effect f (u, v)) e) = (\<Sum>e\<in>incoming w. f e)"
+        and "(\<Sum>e\<in>outgoing w. (push_effect f (u, v)) e) = (\<Sum>e\<in>outgoing w. f e)" by auto   
+      then have "excess (push_effect f e) w = excess f w" unfolding excess_def using obt by simp
+    }
+    thus ?thesis by (metis Diff_iff Un_commute insertI1 insert_is_Un)
+  qed
+  also have "?R2 \<le>  sum l {x \<in> {u, v}. 0 < excess f x} + 2 * card V " (is "_ \<le> ?R2'")
+  proof -
+    have f1: "excess f u > 0" using assms obt unfolding sat_push_precond_def by blast
+    have f2: "excess (push_effect f e) u \<ge> 0" using assms obt push_excess_out 
+      unfolding sat_push_precond_def by auto
+    have f3: "excess (push_effect f e) v > excess f v" 
+    proof -
+      have "excess (push_effect f e) v = excess f v + cf (u, v)"
+        using assms obt push_excess_in unfolding sat_push_precond_def resE_positive by auto
+      moreover have "cf (u, v) > 0" using assms unfolding sat_push_precond_def
+        by (simp add: obt resE_positive)
+      ultimately show ?thesis by simp
+    qed
     
-  
-  
+    let ?set_be = "{x \<in> {u, v}. 0 < excess f x}"  
+    let ?set_af = "{x \<in> {u, v}. 0 < excess (push_effect f e) x}"
+      
+    show ?thesis
+    proof (cases "excess f v > 0"; cases "excess (push_effect f e) u \<noteq> 0")
+      assume "0 < excess f v" and "excess (push_effect f e) u \<noteq> 0"
+      then have "?set_af = {u, v}"
+        and "?set_be = {u, v}" using f1 f2 f3 by auto
+      then have "sum l ?set_af = sum l ?set_be" by auto
+      thus ?thesis by simp
+    next
+      assume "0 < excess f v" and "\<not> excess (push_effect f e) u \<noteq> 0"
+      then have "?set_af = {v}" and "?set_be = {u, v}" using f1 f2 f3 by auto
+      then have " sum l ?set_af = sum l ?set_be - l u " using u_not_v by auto
+      thus ?thesis by simp
+    next
+      assume "\<not> 0 < excess f v" and "excess (push_effect f e) u \<noteq> 0"
+      then have "?set_af \<subseteq> {u, v}" and "{u} \<subseteq> ?set_af" and "?set_be = {u}" using f1 f2 by auto
+      then have "sum l ?set_af \<le> l u + l v" and "sum l ?set_af \<ge> l u"  and "sum l ?set_be = l u"
+        using sum_mono2[of ?set_af "{u}" l]  sum_mono2[of "{u, v}" ?set_af l] u_not_v by auto
+      then have "sum l ?set_af \<ge> sum l ?set_be" and "sum l ?set_af \<le> sum l ?set_be + l v" by auto
+      thus ?thesis using height_bound v_inV by auto
+    next
+      assume "\<not> 0 < excess f v" and "\<not> excess (push_effect f e) u \<noteq> 0"
+      then have "?set_af \<subseteq> {v}" and "?set_be = {u}" using f1 by auto
+      then have "sum l ?set_af \<le> l v" and "sum l ?set_be = l u" 
+        using sum_mono2[of "{v}" ?set_af l] by auto
+      then have "sum l ?set_af \<le> sum l ?set_be - 1"
+        using assms obt unfolding sat_push_precond_def by auto
+      thus ?thesis by simp
+    qed
+  qed   
+  also have "?R1' + ?R2' = unsat_potential f l + 2 * card V"
+    unfolding unsat_potential_def using unsat_pot_decomp[of "\<lambda>x. excess f x > 0"]
+      sum.union_disjoint[symmetric, of "{x \<in> V-{u,v}. 0 < excess f x}""{x \<in> {u,v}. 0 < excess f x}" l]
+    by auto
+  finally show ?thesis by auto
+qed
 end  
   
 context Network begin    
