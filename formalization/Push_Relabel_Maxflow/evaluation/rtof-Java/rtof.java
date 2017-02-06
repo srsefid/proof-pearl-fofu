c06a86bf49a4fc16a94c4c035c9b18049b4edb67
Reza Sefidgar <reza.sefidgar@tum.de>
2017-02-06 17:45:57 +0100

    added Java implementation of Push-Relabel

---
+++ formalization/Push_Relabel_Maxflow/evaluation/rtof-Java/rtof.java
@@ -0,0 +1,63 @@
+import lib.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class rtof {
+    private static PushRelabel initializePR(String path) throws FileNotFoundException {
+        Scanner in = new Scanner(new FileReader(path));
+        int nodeCount = in.nextInt();
+        int edgeCount = in.nextInt();
+
+        PushRelabel prob = new PushRelabel(nodeCount);
+
+
+        for(int i = 0; i < edgeCount; i++) {
+            int u = in.nextInt();
+            int v = in.nextInt();
+            int c = in.nextInt();
+
+            prob.addEdge(u, v, c);
+        }
+
+        return prob;
+    }
+
+    private static MaxFlowResult doTest(PushRelabel G) {
+        int V_size = G.getSize();
+
+        System.gc();
+
+        long startTime = System.currentTimeMillis();
+        long maxflow = G.computeMaxFlow(0, V_size - 1);
+        long endTime  = System.currentTimeMillis();
+
+        return new MaxFlowResult(endTime - startTime, maxflow);
+    }
+
+    // test client that reads input network, solves max flow, and prints results
+    public static void main(String[] args) throws Exception {
+        if (args.length > 2 || args.length < 1) {
+            System.out.println("Usage: [<WARMUP-PATH>] <GRAPH-PATH>");
+        }
+        else {
+            int i=0;
+
+            if (args.length == 2) {
+                // warm up
+                PushRelabel G = initializePR(args[i]);
+                doTest(G);
+                doTest(G);
+                doTest(G);
+                doTest(G);
+                i++;
+            }
+
+            PushRelabel G = initializePR(args[i]);
+            MaxFlowResult result = doTest(G);
+            System.out.format("@@@time: %d ms\n", result.getTime());
+            System.out.format("@@@max-flow: %d\n", result.getFlowValue());
+        }
+    }
+}
