30be29eea21b929bff4181586333c9ac60f778e3
Reza Sefidgar <reza.sefidgar@tum.de>
2017-02-09 19:34:38 +0100

    small change in clean up script

--- formalization/Push_Relabel_Maxflow/evaluation/bench_clean.sh
+++ formalization/Push_Relabel_Maxflow/evaluation/bench_clean.sh
@@ -2,8 +2,11 @@
 
 find . -name "g-*.txt" -type f -delete
 find . -name "*.log" -type f -delete
+find . -name "data-gen" -type f -delete
 find . -name "*.class" -type f -delete
 find . -name "Fofu" -type f -delete
 find . -name "Prpu" -type f -delete
+
+
 
 
