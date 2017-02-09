1d1184f4c3606b44a6566bbfef0750995dc39c63
Reza Sefidgar <reza.sefidgar@tum.de>
2017-02-09 19:26:06 +0100

    Improved cleanup script

--- formalization/Push_Relabel_Maxflow/evaluation/clean.sh
+++ formalization/Push_Relabel_Maxflow/evaluation/clean.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+find . -name "g-*.txt" -type f -delete
 find . -name "*.log" -type f -delete
+find . -name "*.class" -type f -delete
+find . -name "Fofu" -type f -delete
+find . -name "Prpu" -type f -delete
+
+
