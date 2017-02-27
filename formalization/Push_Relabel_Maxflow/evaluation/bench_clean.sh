7a76e5e13f4aa04091f8e4b8981a6f32e48a0742
Reza Sefidgar <reza.sefidgar@tum.de>
2017-02-27 02:07:09 +0100

    enhanced clean up script

--- formalization/Push_Relabel_Maxflow/evaluation/bench_clean.sh
+++ formalization/Push_Relabel_Maxflow/evaluation/bench_clean.sh
@@ -3,6 +3,8 @@
 find . -name "g-*.txt" -type f -delete
 find . -name "*.log" -type f -delete
 find . -name "data-gen" -type f -delete
+find . -name "data-cnv" -type f -delete
+find . -name "gen" -type f -delete
 find . -name "*.class" -type f -delete
 find . -name "Fofu" -type f -delete
 find . -name "Prpu" -type f -delete
