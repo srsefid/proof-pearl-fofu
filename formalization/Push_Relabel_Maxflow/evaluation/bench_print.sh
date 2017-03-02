17171f6052ea6d7714034cdf221c34b499b3f1f5
Reza Sefidgar <reza.sefidgar@tum.de>
2017-03-02 14:08:37 +0100

    new Benchmark results

--- formalization/Push_Relabel_Maxflow/evaluation/bench_print.sh
+++ formalization/Push_Relabel_Maxflow/evaluation/bench_print.sh
@@ -13,13 +13,23 @@ for t in $tests; do
   echo -ne "$t\t& "
   for p in $platforms_fofu; do
     log="fofu-$p/$t.$p.log"
-    time=`grep "@@@time:" $log | sed -re 's/.*@@@time: //g;s/ms//g'`
+    time="-1"
+    if [ -e $log ]; then
+      if [ -n "$(grep "@@@time:" $log)" ]; then
+        time=`grep "@@@time:" $log | sed -re 's/.*@@@time: //g;s/ms//g'`
+      fi
+    fi    
     echo -ne "$time\t& "
   done
   
   for p in $platforms_prpu; do
     log="prpu-$p/$t.$p.log"
-    time=`grep "@@@time:" $log | sed -re 's/.*@@@time: //g;s/ms//g'`
+    time="-1"
+    if [ -e $log ]; then
+      if [ -n "$(grep "@@@time:" $log)" ]; then
+        time=`grep "@@@time:" $log | sed -re 's/.*@@@time: //g;s/ms//g'`
+      fi
+    fi 
     echo -ne "$time\t& "
   done
 
